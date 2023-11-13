f6dd0b31a903054dd9a959ac36f79c8846e64af8
Shawn Hutchinson <shutch@ksu.edu>
2023-11-13 09:51:09 -0600

    Add files via upload

---
+++ GitHub-OtherCursors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    File name: GitHub-OtherCursors.py
+    Author: ENTER YOUR  NAME
+    Description:  ENTER BRIEF DESCRIPTION
+    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    Python Version: 3.9.16
+"""
+
+# Import arcpy module and allow overwrites
+import arcpy
