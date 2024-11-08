3321d8e6cc3e601840ca8e4433d951a37514d560
nishadiniwidanagamage <nishadini@ksu.edu>
2024-11-07 18:36:28 -0600

    Update GitHub-Rasters.py

--- GitHub-Rasters.py
+++ GitHub-Rasters.py
@@ -3,11 +3,51 @@
 
 """
     File name: GitHub-Rasters.py
-    Author: ENTER YOUR  NAME
+    Author: Nishadini Widanagamage
     Description:  ENTER BRIEF DESCRIPTION
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT
+    Date created: 11/06/2024
     Python Version: 3.9.16
 """
 
 # Import arcpy module and allow overwrites
 import arcpy
+import os
+from arcpy.sa import Minus, Plus, Divide
+
+# Set workspace and allow overwrite output
+arcpy.env.workspace = r"C:\Users\nishadini\OneDrive - Kansas State University\Documents\GitHub\rasters-nishadiniwidanagamage-main\ExerciseData.gdb"
+arcpy.env.overwriteOutput = True
+
+# Establish local variables 
+in_raster_r = "LANDSAT8_20160510/Band_4" # Red band for Landsat 8
+in_raster_nir = "LANDSAT8_20160510/Band_5" # NIR band for Landsat 8
+out_workspace = r"C:\Users\nishadini\OneDrive - Kansas State University\Documents\GitHub\rasters-nishadiniwidanagamage-main\scratch.gdb"
+
+
+# Error trapping using try-except 
+try:
+    # Checking the availability of required extensions
+    if arcpy.CheckExtension("Spatial") == "Available":
+        # Checking out required extensions 
+        arcpy.CheckOutExtension("Spatial")
+
+        # Rescale bands since unrescaled bands were giving NDVI range -1 to 0.
+        red_band = Divide(arcpy.Raster(in_raster_r), 10000)
+        nir_band = Divide(arcpy.Raster(in_raster_nir), 10000)
+
+        # Calculate NDVI
+        ndvi = Divide(Minus(nir_band, red_band), Plus(nir_band, red_band))
+
+        # Save NDVI output
+        ndvi.save(os.path.join(out_workspace, "NDVI")) 
+        print("NDVI calculated and saved")
+
+        # Checking in used extensions 
+        arcpy.CheckInExtension("Spatial")
+
+    else:
+        print("Spatial extension is not available")
+    
+except arcpy.ExecuteError:
+    print(arcpy.GetMessages(2))
+
